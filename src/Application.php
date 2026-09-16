49d1b5a73d94068b4f460751cbe94ec655bcd640
watsonhaw5566 <348748267@qq.com>
2026-09-16 15:53:52 +0800

    feat: command 命令新增 route:list

--- src/Application.php
+++ src/Application.php
@@ -130,6 +130,7 @@ class Application
         $this->container->singleton(ConsoleApplication::class, function (): ConsoleApplication {
             $console = new ConsoleApplication($this->container);
             $console->addCommand(\Lychee\console\command\ServeCommand::class);
+            $console->addCommand(\Lychee\console\command\RouteListCommand::class);
 
             return $console;
         });
