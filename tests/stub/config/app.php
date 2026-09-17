76b54fea2a86a33d7b49e4cc4b6979abd955ca12
watsonhaw5566 <348748267@qq.com>
2026-09-17 16:09:08 +0800

    style: 风格化代码

--- tests/stub/config/app.php
+++ tests/stub/config/app.php
@@ -4,16 +4,16 @@ declare(strict_types=1);
 
 return [
     // 默认时区
-    'default_timezone' => 'Asia/Shanghai',
+    'default_timezone'  => 'Asia/Shanghai',
 
     // 路由全局前缀，API 开发时可设置为 'api'，所有路由自动加上此前缀
-    'route_prefix'     => '',
+    'route_prefix'      => '',
 
     // 错误显示信息，非调试模式有效
-    'error_message'    => '页面错误，请稍后再试~',
+    'error_message'     => '页面错误，请稍后再试~',
 
     // 是否显示错误信息（非调试模式下是否暴露真实异常信息）
-    'show_error_msg'   => false,
+    'show_error_msg'    => false,
 
     // 异常处理器类名，需继承 Lychee\http\ExceptionHandler
     // 留空或不配置时使用框架默认处理器
