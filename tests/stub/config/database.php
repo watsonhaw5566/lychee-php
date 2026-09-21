363a98fa3beff4558deb8682496cbab268f21688
watsonhaw <watsonhaw@hotmail.com>
2026-09-21 10:22:20 +0800

    feat(database): 支持 fields_strict 配置 (#83)
    * feat: 支持 fields_strict 配置
    
    * feat: 支持 fields_strict 配置
    
    ---------
    
    Co-authored-by: watsonhaw5566 <348748267@qq.com>

--- tests/stub/config/database.php
+++ tests/stub/config/database.php
@@ -9,22 +9,34 @@ return [
     // 连接列表
     'connections' => [
         'sqlite' => [
-            'type'     => 'sqlite',
-            'database' => ':memory:',
-            'prefix'   => '',
-            'debug'    => false,
+            'type'          => 'sqlite',
+            'database'      => ':memory:',
+            'prefix'        => '',
+            'debug'         => false,
+            // 是否严格检查字段是否存在（默认 true）
+            // true：写入/更新时若包含数据表不存在的字段会抛出异常
+            // false：忽略不存在的字段
+            'fields_strict' => true,
         ],
 
         'mysql'  => [
-            'type'     => 'mysql',
-            'hostname' => '127.0.0.1',
-            'hostport' => 3306,
-            'database' => '',
-            'username' => 'root',
-            'password' => '',
-            'charset'  => 'utf8mb4',
-            'prefix'   => '',
-            'debug'    => false,
+            'type'            => 'mysql',
+            'hostname'        => '127.0.0.1',
+            'hostport'        => 3306,
+            'database'        => '',
+            'username'        => 'root',
+            'password'        => '',
+            'charset'         => 'utf8mb4',
+            'prefix'          => '',
+            'debug'           => false,
+            // 是否严格检查字段是否存在（默认 true）
+            'fields_strict'   => true,
+            // 是否需要断线重连
+            'break_reconnect' => false,
+            // 监听 SQL
+            'trigger_sql'     => true,
+            // 开启字段缓存
+            'fields_cache'    => false,
         ],
     ],
 ];
